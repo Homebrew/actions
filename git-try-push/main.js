63dce1cb71ae62477f3deff227b1f6c218f748f0
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-29 16:35:42 +0200

    git-try-push: first checkout, then pull and push

--- git-try-push/main.js
+++ git-try-push/main.js
@@ -20,17 +20,21 @@ async function main() {
             await exec.exec("git", ["config", "--local", "http.https://github.com/.extraheader", `AUTHORIZATION: basic x-access-token:${token}`])
         }
 
+        // Checkout the branch which should be pushed.
+        await exec.exec("git", ["checkout", branch])
+
         // Loop specified number of tries.
         for (let i = 0; i < tries; i++) {
             try {
-                // Try to push, if successful, then just exit.
-                await exec.exec("git", ["push", remote, branch + ":" + branch])
+                // Try to push, if successful, then checkout previous branch and just exit.
+                await exec.exec("git", ["push", remote, branch])
+                await exec.exec("git", ["checkout", "-"])
                 return
             } catch (error) {
                 // Push failed. Wait some time, pull changes with rebasing and try again.
                 const delay = Math.floor(Math.random() * (10 + i)) + 3
                 await exec.exec("sleep", [delay])
-                await exec.exec("git", ["pull", "--rebase", remote, branch + ":" + branch])
+                await exec.exec("git", ["pull", "--rebase", remote, branch])
             }
         }
 
