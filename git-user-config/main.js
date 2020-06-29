9e4009b37e436080d9d1136951eb187945269a9a
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-29 17:02:30 +0200

    Add git-user-config Action

---
+++ git-user-config/main.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+async function main() {
+    try {
+        const token = core.getInput("token", { required: true })
+        const username = core.getInput("username", { required: true })
+
+        const client = github.getOctokit(token)
+
+        const user = await client.users.getByUsername({
+            username: username
+        })
+        const name = (user.data.name || user.data.login)
+        const email = (user.data.email || user.data.id + "+" + user.data.login + "@users.noreply.github.com")
+
+        core.setOutput("name", name)
+        core.setOutput("email", email)
+
+        await exec.exec("git", ["config", "--global", "user.name", name])
+        await exec.exec("git", ["config", "--global", "user.email", email])
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
