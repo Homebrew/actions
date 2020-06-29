b242df072f6cb9ce9cb657980cacdae740d6c73d
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-29 10:58:12 +0200

    git-try-push: tweak inputs

--- git-try-push/main.js
+++ git-try-push/main.js
@@ -4,11 +4,12 @@ const exec = require('@actions/exec')
 async function main() {
     try {
         const token = core.getInput("token")
-        const remote = core.getInput("remote")
-        const branch = core.getInput("branch")
-        const tries = core.getInput("tries")
         const directory = core.getInput("directory")
+        const remote = core.getInput("remote", { required: true })
+        const branch = core.getInput("branch", { required: true })
+        const tries = core.getInput("tries", { required: true })
 
+        // Change directory.
         if (directory) {
             process.chdir(directory)
         }
@@ -19,16 +20,17 @@ async function main() {
             await exec.exec("git", ["config", "--local", "http.https://github.com/.extraheader", `AUTHORIZATION: basic x-access-token:${token}`])
         }
 
+        // Loop specified number of tries.
         for (let i = 0; i < tries; i++) {
             try {
                 // Try to push, if successful, then just exit.
-                await exec.exec("git", ["push", remote, branch+":"+branch])
+                await exec.exec("git", ["push", remote, branch + ":" + branch])
                 return
             } catch (error) {
                 // Push failed. Wait some time, pull changes with rebasing and try again.
                 const delay = Math.floor(Math.random() * (10 + i)) + 3
                 await exec.exec("sleep", [delay])
-                await exec.exec("git", ["pull", "--rebase", remote, branch+":"+branch])
+                await exec.exec("git", ["pull", "--rebase", remote, branch + ":" + branch])
             }
         }
 
