185b7ad52f30ca373fa2ab4b1b3dae7aac8a4d74
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-30 11:35:20 +0200

    Add post-comment Action

---
+++ post-comment/main.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function main() {
+    try {
+        const token = core.getInput("token", { required: true })
+        const issue = core.getInput("issue", { required: true })
+        const body = core.getInput("body")
+        const bot_body = core.getInput("bot_body")
+        const bot = core.getInput("bot")
+
+        const client = github.getOctokit(token)
+
+        await client.issues.createComment({
+            ...github.context.repo,
+            issue_number: issue,
+            body: github.context.actor == bot ? bot_body : body
+        })
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
