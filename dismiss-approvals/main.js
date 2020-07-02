c9aab49d08b6e8bd965ee5645760b4ac0fd03b3f
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-02 21:51:08 +0200

    Add dismiss-approvals Action

---
+++ dismiss-approvals/main.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function main() {
+    try {
+        const token = core.getInput("token", { required: true })
+        const pr = core.getInput("pr", { required: true })
+        const message = core.getInput("message", { required: true })
+
+        const client = github.getOctokit(token)
+
+        const reviews = await client.pulls.listReviews({
+            ...github.context.repo,
+            pull_number: pr
+        })
+
+        for (const review of reviews.data) {
+            if (review.state != "APPROVED")
+                continue
+
+            client.pulls.dismissReview({
+                ...github.context.repo,
+                pull_number: pr,
+                review_id: review.id,
+                message: message
+            });
+        }
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
