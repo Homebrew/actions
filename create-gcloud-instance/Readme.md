575dd7899f200e81acafc54d7d714aa174726cc6
Michka Popoff <michkapopoff@gmail.com>
2021-05-06 22:57:54 +0200

    create-gcloud-instance (new action)

---
+++ create-gcloud-instance/Readme.md
@@ -0,0 +1,18 @@
+# Create a new linux self-hosted runner GitHub Action
+
+An action that creates a new self-hosted runner using gcloud.
+
+The GITHUB_TOKEN has to have "public repo" access.
+
+## Usage
+
+```yaml
+- name: Create a new linux self-hosted runner
+  uses: Homebrew/actions/create-gcloud-instance@master
+  with:
+    runner_name: linux-self-hosted-1
+    gcp_project_id: ${{ secrets.GCP_PROJECT_ID }}
+    gcp_service_account: ${{ secrets.GCP_SERVICE_ACCOUNT }}
+    gcp_sa_key: ${{ secrets.GCP_SA_KEY }}
+    github_token: ${{ secrets.HOMEBREW_GITHUB_PUBLIC_REPO_TOKEN }}
+```
---
+++ create-gcloud-instance/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -xeo pipefail
+
+RUNNER_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/RUNNER_NAME -H "Metadata-Flavor: Google")
+VM_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/VM_TOKEN -H "Metadata-Flavor: Google")
+REPO_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/REPO_NAME -H "Metadata-Flavor: Google")
+GITHUB_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/GITHUB_TOKEN -H "Metadata-Flavor: Google")
+
+# Initial setup: setup docker and github actions runner.
+# This is done only once, if the machine is stopped/started, this block of code is not executed again.
+if [ ! -d "/home/actions" ]; then
+    # Setup docker
+    apt-get -y update && apt-get install -y curl apt-transport-https ca-certificates software-properties-common jq wget
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    apt-get update
+
+    apt-get install -y docker-ce
+
+    useradd -m actions
+
+    # Add user to docker group and restart docker to make group take effect
+    usermod -aG docker actions
+    newgrp docker
+    systemctl restart docker
+
+    curl -o /home/actions/install_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/install_runner.sh
+    curl -o /home/actions/config_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/config_runner.sh
+    chmod +x /home/actions/install_runner.sh
+    chmod +x /home/actions/config_runner.sh
+
+    # This needs to be run with the actions user:
+    su -c "/home/actions/install_runner.sh" actions
+
+    # This needs to be run as root:
+    # shellcheck disable=SC1091
+    source /home/actions/actions-runner/bin/installdependencies.sh
+
+    # This needs to be run with the actions user:
+    RUNNER_NAME=${RUNNER_NAME} VM_TOKEN=${VM_TOKEN} REPO_NAME=${REPO_NAME} GITHUB_TOKEN=${GITHUB_TOKEN} su -c "/home/actions/config_runner.sh" actions
+fi
+
+curl -o /home/actions/start_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/start_runner.sh
+chmod +x /home/actions/start_runner.sh
+
+su -p -c "/home/actions/start_runner.sh" actions
