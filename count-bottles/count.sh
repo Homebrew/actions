94b8d18d131abbd9763335c82366d1beee6bcdee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-05-26 17:17:32 +0800

    count-bottles, post-build: updates for dependent tests caching
    Needed for Homebrew/homebrew-test-bot#927.

--- count-bottles/count.sh
+++ count-bottles/count.sh
@@ -14,15 +14,20 @@ shopt -s nullglob
 
 json_files=(*.json)
 count="${#json_files[@]}"
-echo "::notice ::${count} bottles"
+echo "::notice ::${count} bottle(s)"
 echo "count=${count}" >> "${GITHUB_OUTPUT:?}"
 
 failed_json_files=(failed/*.json)
 failures="${#failed_json_files[@]}"
-echo "::notice ::${failures} failed bottles"
+echo "::notice ::${failures} failed bottle(s)"
 echo "failures=${failures}" >> "${GITHUB_OUTPUT:?}"
 
 skipped_lists=(skipped_or_failed_formulae-*.txt)
 skipped="${#skipped_lists[@]}"
-echo "::notice ::${skipped} lists of skipped formulae"
+echo "::notice ::${skipped} list(s) of skipped formulae"
 echo "skipped=${skipped}" >> "${GITHUB_OUTPUT:?}"
+
+dependents_lists=(tested-dependents-*.txt)
+dependents="${#dependents_lists[@]}"
+echo "::notice ::${dependents} list(s) of successfully tested dependents"
+echo "dependents=${dependents}" >> "${GITHUB_OUTPUT:?}"
