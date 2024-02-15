5caa94335a28d8fdf5a478ae8586f2da40a0a989
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-02-15 22:19:20 +0800

    setup-homebrew: use `/bin/bash`

--- setup-homebrew/post.mjs
+++ setup-homebrew/post.mjs
@@ -3,7 +3,7 @@ import core from "@actions/core"
 
 // GitHub Actions does not support shell `post` actions and thus requires a JS wrapper.
 try {
-  await exec("bash", [
+  await exec("/bin/bash", [
     new URL("./post.sh", import.meta.url).pathname,
     core.getInput("debug")
   ])
