ea20c5e7c9ebc8266c5687e9c4e1bb0160879fc8
Bevan Kay <email@bevankay.me>
2024-06-17 15:19:26 +1000

    remove-disabled-packages: add action

---
+++ remove-disabled-packages/main.rb
@@ -0,0 +1,70 @@
+# typed: true
+# frozen_string_literal: true
+
+require "formula"
+require "cask"
+
+# The RemoveDisabledPackages class finds packages that have been disabled for over 12 months
+# and creates commits in the local repository to remove them.
+class RemoveDisabledPackages
+  ONE_YEAR_AGO = (Date.today << 12).freeze
+
+  def initialize(target_tap)
+    @target_tap = target_tap
+  end
+
+  def run
+    packages_to_remove = find_disabled(packages: Formula.all + Cask::Cask.all)
+
+    puts "Removing old packages..."
+
+    packages_to_remove.each { |package| FileUtils.rm sourcefile_path(package) }
+
+    tap_dir = @target_tap.path
+
+    out, err, status = Open3.capture3 "git", "-C", tap_dir.to_s, "status", "--porcelain", "--ignore-submodules=dirty"
+    raise err unless status.success?
+
+    if out.chomp.empty?
+      puts "No packages removed."
+      File.open(ENV.fetch("GITHUB_OUTPUT", nil), "a") { |f| f.puts("packages-removed=false") }
+      exit
+    end
+
+    git "-C", tap_dir.to_s, "add", "--all"
+
+    packages_to_remove.each do |package|
+      name = package.is_a?(Formula) ? package.name : package.token
+      puts "Removed `#{name}`."
+      git "-C", tap_dir.to_s, "commit", sourcefile_path(package), "--message",
+          "#{name}: remove #{package.is_a?(Formula) ? "formula" : "cask"}", "--quiet"
+    end
+
+    File.open(ENV.fetch("GITHUB_OUTPUT", nil), "a") { |f| f.puts("packages-removed=true") }
+  end
+
+  private
+
+  def git(*args)
+    system "git", *args
+    exit $CHILD_STATUS.exitstatus unless $CHILD_STATUS.success?
+  end
+
+  def find_disabled(packages: [])
+    puts "Finding disabled packages..."
+    packages.select do |package|
+      next false if package.tap != @target_tap
+      next false unless package.disabled?
+      next false if package.disable_date.nil?
+
+      package.disable_date < ONE_YEAR_AGO
+    end
+  end
+
+  def sourcefile_path(package)
+    package.is_a?(Formula) ? package.path : package.sourcefile_path
+  end
+end
+
+target_tap = Tap.fetch(ENV.fetch("GITHUB_REPOSITORY"))
+RemoveDisabledPackages.new(target_tap).run
