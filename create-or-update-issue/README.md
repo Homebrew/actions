5b99a85d48827b53905d7d0887843ddca359ad73
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-09-18 16:58:54 +0800

    create-or-update-issue: add action
    Related: https://github.com/Homebrew/homebrew-core/pull/190657#issuecomment-2353378549
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>
    Co-authored-by: Bo Anderson <mail@boanderson.me>

---
+++ create-or-update-issue/README.md
@@ -0,0 +1,24 @@
+# Create Or Update Issue
+
+An action to create or update an issue in a repository.
+It supports posting a comment under an existing issue with the same title or
+closing it based on the outcome of a previous step.
+
+## Usage
+
+```yaml
+- uses: Homebrew/actions/create-or-update-issue@master
+  with:
+    token: ${{ github.token }} # defaults to this
+    repository: ${{ github.repository }} # defaults to this
+    title: Issue title
+    body: Issue body
+    labels: label1,label2 # optional
+    assignees: user1,user2 # optional
+    # If true: post `body` as a comment under the issue with the same title, if
+    # such an issue is found; otherwise, create a new issue.
+    update-existing: ${{ steps.<step-id>.conclusion == 'failure' }}
+    # If true: close an existing issue with the same title as completed, if such
+    # an issue is found; otherwise, do nothing.
+    close-existing: ${{ steps.<step-id>.conclusion == 'success' }}
+```
---
+++ create-or-update-issue/main.test.mjs
@@ -0,0 +1,136 @@
+import util from "node:util";
+
+describe("create-issue", async () => {
+  const token = "fake-token";
+  const title = "Issue title";
+  const body = "Issue body.\nLorem ipsum dolor sit amet.";
+  const labels = "label1,label2";
+  const assignees = "assignee1,assignee2";
+
+  const issueNumber = 12345;
+
+  beforeEach(async () => {
+    mockInput("token", token);
+    mockInput("repository", GITHUB_REPOSITORY);
+    mockInput("title", title);
+    mockInput("body", body);
+    mockInput("labels", labels);
+    mockInput("assignees", assignees);
+  });
+
+  it("creates an issue", async () => {
+    mockInput("update-existing", "false");
+    mockInput("close-existing", "false");
+
+    const mockPool = githubMockPool();
+
+    mockPool.intercept({
+      method: "POST",
+      path: `/repos/${GITHUB_REPOSITORY}/issues`,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+      body: (htmlBody) => util.isDeepStrictEqual(JSON.parse(htmlBody), {
+        title,
+        body,
+        labels: labels.split(","),
+        assignees: assignees.split(","),
+      }),
+    }).defaultReplyHeaders({
+      "Content-Type": "application/json",
+    }).reply(200, {
+      number: issueNumber,
+    });
+
+    await loadMain();
+  });
+
+  it("for advanced use case with `close-existing: true`", async () => {
+    mockInput("update-existing", "true");
+    mockInput("close-existing", "false");
+
+    const mockPool = githubMockPool();
+
+    mockPool.intercept({
+      method: "GET",
+      path: `/repos/${GITHUB_REPOSITORY}/issues?` +
+        `direction=desc&per_page=100&sort=created&state=open`,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    }).defaultReplyHeaders({
+      "Content-Type": "application/json",
+    }).reply(200, [
+      {
+        title: "Not the same issue",
+        number: 54321,
+      },
+      {
+        title,
+        number: issueNumber,
+      },
+    ]);
+
+    mockPool.intercept({
+      method: "POST",
+      path: `/repos/${GITHUB_REPOSITORY}/issues/${issueNumber}/comments`,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+      body: (htmlBody) => util.isDeepStrictEqual(JSON.parse(htmlBody), {
+        body,
+      }),
+    }).defaultReplyHeaders({
+      "Content-Type": "application/json",
+    }).reply(200, {
+      html_url: "https://github.com/owner/repo/issues/12345#issuecomment-67890",
+    });
+
+    await loadMain();
+  });
+
+  it("for advanced use case with `close-existing: true`", async () => {
+    mockInput("update-existing", "false");
+    mockInput("close-existing", "true");
+
+    const mockPool = githubMockPool();
+
+    mockPool.intercept({
+      method: "GET",
+      path: `/repos/${GITHUB_REPOSITORY}/issues?` +
+        `direction=desc&per_page=100&sort=created&state=open`,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    }).defaultReplyHeaders({
+      "Content-Type": "application/json",
+    }).reply(200, [
+      {
+        title: "Not the same issue",
+        number: 54321,
+      },
+      {
+        title,
+        number: issueNumber,
+      },
+    ]);
+
+    mockPool.intercept({
+      method: "PATCH",
+      path: `/repos/${GITHUB_REPOSITORY}/issues/${issueNumber}`,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+      body: (htmlBody) => util.isDeepStrictEqual(JSON.parse(htmlBody), {
+        state: "closed",
+        state_reason: "completed",
+      }),
+    }).defaultReplyHeaders({
+      "Content-Type": "application/json",
+    }).reply(200, {
+      html_url: "https://github.com/owner/repo/issues/12345#issuecomment-67890",
+    });
+
+    await loadMain();
+  });
+});
