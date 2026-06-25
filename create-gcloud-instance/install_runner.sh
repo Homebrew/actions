7bc8b1ec22c08ae922e1438b33ff526d8bba395e
Mike McQuaid <mike@mikemcquaid.com>
2026-06-25 12:18:08 +0100

    Delete create-gcloud-instance
    We're no longer using Google Cloud.
    
    # Conflicts:
    #	create-gcloud-instance/create-gcloud-instance.sh
    #	create-gcloud-instance/setup.sh

--- create-gcloud-instance/install_runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-mkdir -p /home/actions/actions-runner
-curl https://api.github.com/repos/actions/runner/releases/latest |
-  grep -E -o 'https://github.com/actions/runner/releases/download/v[0-9.]+/actions-runner-linux-x64-[0-9.]+\.tar\.gz' |
-  uniq |
-  xargs curl -L |
-  tar xzv -C /home/actions/actions-runner
